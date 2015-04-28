96200a09077e50d3c6a48def77aeb4565bda80f5
tim <tim@magnetic.io>
2015-04-28 10:36:52 +0200

    bump version number

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ var (
 	pidFilePath      string
 	headless         bool
 	log              *gologger.Logger
-	version          = "0.7.0-RC3"
+	version          = "0.7.5"
 	stream           metrics.Streamer
 	workDir          helpers.WorkDir
 	customWorkDir    string
