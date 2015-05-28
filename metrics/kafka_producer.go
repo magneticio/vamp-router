b493d3ea331d482bcc0f079ebbf66fa7fdb08c6d
tim <tim@magnetic.io>
2015-05-28 12:49:33 +0200

    updates to the Kafka package

--- metrics/kafka_producer.go
+++ metrics/kafka_producer.go
@@ -23,50 +23,39 @@ func (k *KafkaProducer) Start(host string, port int) {
 
 	k.Log.Notice("Connecting to Kafka on " + connection + "...")
 
-	clientConfig := sarama.NewClientConfig()
-	clientConfig.WaitForElection = (10 * time.Second)
-
-	client, err := sarama.NewClient("client_id", []string{connection}, clientConfig)
-	if err != nil {
-		panic(err)
-	} else {
-		k.Log.Notice("Connection to Kafka successful")
-	}
+	config := sarama.NewConfig()
+	config.Metadata.Retry.Backoff = (10 * time.Second)
 
 	/**
-	 *  Create a producer with some specific setting
+	 *  Set producer config
 	 */
-	producerConfig := sarama.NewProducerConfig()
-
-	// if delivering messages async,  buffer them for at most MaxBufferTime
-	producerConfig.MaxBufferTime = (2 * time.Second)
-
-	// max bytes in buffer
-	producerConfig.MaxBufferedBytes = 51200
-
-	// Use zip compression
-	producerConfig.Compression = 0
+	// don't use zip compression
+	config.Producer.Compression = 0
 
 	// We are just streaming metrics, so don't not wait for any Kafka Acks.
-	producerConfig.RequiredAcks = -1
+	config.Producer.RequiredAcks = -1
 
-	producer, err := sarama.NewProducer(client, producerConfig)
+	producer, err := sarama.NewSyncProducer([]string{connection}, config)
 	if err != nil {
-		panic(err)
+
+		k.Log.Error("Error connecting to Kafka: ", err.Error())
+	} else {
+		k.Log.Notice("Connection to Kafka successful")
 	}
 
 	go k.produce(producer)
 
 }
 
-func (k *KafkaProducer) produce(producer *sarama.Producer) {
+func (k *KafkaProducer) produce(producer sarama.SyncProducer) {
 	for {
 		metric := <-k.metricsChannel
 		json, err := json.MarshalIndent(metric, "", " ")
 		if err != nil {
 			return
 		}
-		err = producer.SendMessage("loadbalancer.all", sarama.StringEncoder("lbmetrics"), sarama.StringEncoder(json))
+		msg := &sarama.ProducerMessage{Topic: "loadbalancer.all", Value: sarama.StringEncoder(json)}
+		_, _, err = producer.SendMessage(msg)
 		if err != nil {
 			k.Log.Error("error sending to Kafka ")
 		}
